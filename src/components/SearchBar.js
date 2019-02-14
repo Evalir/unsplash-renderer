cda2ee9ae936d1594d009e837d312301a1a35418
evalir.io <hi.evalir@gmail.com>
2019-02-14 12:12:37 -0400

    Finish searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ class SearchBar extends React.Component {
     //transform to arrow function to fix this binding problem, or pass callback to keep this as a normal function
     onFormSubmit = e => {
         e.preventDefault()
-        this.props.onSubmit(this.state.term) //get props on a class component
+        this.props.onSubmit(this.state.term) //this calls a callback function passed by its parent, with the parameter that it wants
     }
 
     render() {
